368143c675ae8e9e46b1f88a8b03088fb16af81a
Joseph Khella <josephkhella2010@gmail.com>
2026-02-25 21:14:43 +0100

    fix five

--- backend/backend_chat/apps.py
+++ backend/backend_chat/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
 
-class ApiConfig(AppConfig):
+from django.apps import AppConfig
+
+class BackendChatConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api'
+    name = 'backend_chat'
