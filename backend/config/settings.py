32142af8239ade805ca62467d96b8a959e878c78
Joseph Khella <josephkhella2010@gmail.com>
2026-02-25 19:05:48 +0100

    change two

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'django-insecure-ue$-fpbu!u(kdc$(9y^8zf^3b_mr%n_gj&54$p+g0ae6)*fipa'
-SECRET_KEY = os.environ["SECRET_KEY"]
-
+SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
@@ -43,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
