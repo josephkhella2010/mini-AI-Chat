ae5689096baf9cb271193ed33dc1e9ba37c49d28
Joseph Khella <josephkhella2010@gmail.com>
2026-02-26 19:46:35 +0100

    add get users

--- backend/backend_chat/api_routes/get_users.py
+++ backend/backend_chat/api_routes/get_users.py
@@ -1,25 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from ..models import User
 from django.views.decorators.csrf import csrf_exempt
 
-Users=[
-    {
-    "name":"jesus",
-    "age":2026,
-    "id":1
-},
-    {
-    "name":"maria",
-    "age":2040,
-    "id":2
-},
-   {
-    "name":"joseph",
-    "age":2100,
-    "id":3
-},
-
-]
 
 @csrf_exempt
 def get_users(req):
@@ -27,8 +10,15 @@ def get_users(req):
         return JsonResponse({"error":"Method not Allowed"},status=405)
     
     try:
-        formate_Users=list(map(lambda u:{"id":u["id"],"name":u["name"],"age":u["age"]},Users))
-        return JsonResponse({"users":formate_Users},status=200)
+        usersdb=User.objects.all()
+        users=list(map(lambda u:{"id":u.id,"username":u.username,"email":u.email
+                                 ,"firstname":u.firstname,
+                                 "lastname":u.lastname,
+                                 "dateOfBirth":u.dateOfBirth,
+                                 "password":u.password,
+                                 "items":(list(map(lambda it: {"id":it.id,"name":it.name},u.items.all())))
+                                 },usersdb))
+        return JsonResponse({"users":users},status=200)
     
     except Exception as e:
         return JsonResponse({"error":str(e)},status=500)
