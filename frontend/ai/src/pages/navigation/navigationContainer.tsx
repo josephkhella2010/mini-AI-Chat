2366804035e984475856af9313da621fdcff1fe2
Joseph Khella <josephkhella2010@gmail.com>
2026-02-27 15:37:49 +0100

    fix error

--- frontend/ai/src/pages/navigation/navigationContainer.tsx
+++ frontend/ai/src/pages/navigation/navigationContainer.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 
-export default function navigationContainer() {
+export default function NavigationContainer() {
   const navigate = useNavigate();
   return (
     <div>
--- frontend/ai/src/pages/routes/RoutesPage.tsx
+++ frontend/ai/src/pages/routes/RoutesPage.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "../Home/HomePage";
 import RegisterPage from "../register/RegisterPage";
+import NavigationContainer from "../navigation/navigationContainer";
 
 export default function RoutesPage() {
   return (
     <div>
+      <NavigationContainer />
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
