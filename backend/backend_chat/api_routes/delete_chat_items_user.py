64f5d7112dfbb095c79cefa3cb7370472ff7bf36
Joseph Khella <josephkhella2010@gmail.com>
2026-03-07 20:45:12 +0100

    add delete api for delete items

---
+++ backend/backend_chat/api_routes/delete_chat_items_user.py
@@ -0,0 +1,72 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from ..models import User
+from ..models import Chat
+from django.db.models import Q
+from ..auth.jwt import decode_jwt
+
+@csrf_exempt
+def delete_chat_items_user(req,user_id,chat_id):
+    if req.method !="DELETE":
+        JsonResponse({"error":"Method is not valid"},status=405)
+
+
+    try:
+         # 🔐 Authentication
+        auth_header = req.headers.get("Authorization")
+
+        if not auth_header or not auth_header.startswith("Bearer "):
+            return JsonResponse({"error": "Authorization token required"}, status=401)
+
+        token = auth_header.split(" ")[1]
+        payload = decode_jwt(token)
+
+        if not payload:
+            return JsonResponse({"error": "Invalid or expired token"}, status=401)
+
+        token_user_id = payload.get("user_id")
+
+        # 🔒 Authorization check
+        if int(token_user_id) != int(user_id):
+            return JsonResponse(
+                {"error": "You are not allowed to add items for this user"},
+                status=403
+            )
+
+        token_user = User.objects.filter(id=token_user_id).first()
+
+        if not token_user:
+            return JsonResponse({"error": "User not found"}, status=404)
+            #  Find chat
+        chat = Chat.objects.filter(id=chat_id, user=token_user).first()
+        if not chat:
+            return JsonResponse({"error": "Chat not found"}, status=404)
+
+        chat.delete()
+        user_items=[{
+            "chatId":c.id,
+            "chat_items":[{
+                  "id": item.id,
+                   "question": item.question,
+                    "answer": item.answer
+
+            }for item in c.chatItems.all()
+                      
+            ] 
+
+        } for c in  token_user.items.all()
+        ]  
+        updated_user={
+            "id": token_user.id,
+            "firstname": token_user.firstname,
+            "lastname": token_user.lastname,
+            "email": token_user.email,
+            "username": token_user.username,
+            "items": user_items
+
+        }
+        JsonResponse({"msg":"successfully deleted","user":updated_user,"items":user_items},status=200)
+    except Exception as e:
+        JsonResponse({"error":str(e)},status=500)
--- backend/backend_chat/api_routes/urls.py
+++ backend/backend_chat/api_routes/urls.py
@@ -7,6 +7,7 @@ from .update_user import update_user
 from .add_item_to_chat import add_item_to_chat
 from .add_chat_to_user import add_chat_to_user
 from .get_user_chat_items import get_user_chat_items
+from .delete_chat_items_user import delete_chat_items_user
 urlpatterns = [
     path('users/', get_users),
     path('register-user', register_user),
@@ -16,5 +17,6 @@ urlpatterns = [
     path("add-chat/userId=<int:user_id>",  add_chat_to_user ) , 
     path("get-user-chats/userId=<int:user_id>",  get_user_chat_items ) , 
     path("add-chat-item/userId=<int:user_id>/chatId=<int:chat_id>",add_item_to_chat ) , 
+    path("delete-chat-item/userId=<int:user_id>/chatId=<int:chat_id>",delete_chat_items_user),
 
 ]
